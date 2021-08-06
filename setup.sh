eb0ec8327a024c1f294aedd238bcb9d6bb63ebef
watasuke102 <watasuke102@gmail.com>
2021-08-06 12:02:04 +0900

    fix: deinがインストールできなかった問題

--- setup.sh
+++ setup.sh
@@ -25,7 +25,9 @@ current=$(cd $(dirname ${0}); pwd)
 
 echo -e "\033[36m[info] Downloading dependences...\033[39m"
 [[ ! -e ~/.git-prompt.sh ]] && curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh 
-[[ ! -e ~/.cache/dein ]] && curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh | sh - ~/.cache/dein
+
+installer_tmp=$(mktemp)
+[[ ! -e ~/.cache/dein ]] && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${installer_tmp} | sh ${installer_tmp} ~/.cache/dein
 
 echo -e "\033[36m[info] Creating symlink...\033[39m"
 ln -snvf  ${current}/.zshrc     ~/.zshrc

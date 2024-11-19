f37ce6054b2bdee86773bc3cb3d754a6e6e22a40
watasuke102 <watasuke102@gmail.com>
2024-11-19 21:53:09 +0900

    update: neovim>keymap that toggle terminal

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -17,3 +17,30 @@ map("n", "<leader>l", "<cmd>bnext<CR>", o)
 map("n", "<C-j>", "ddp", o)
 map("n", "<C-k>", "ddkP", o)
 
+-- toggle terminal by:
+-- https://github.com/3BD-R/nvim/blob/main/lua/basic/term.lua
+local term_win_id = nil
+local term_buffer = nil
+local cmd = vim.api.nvim_command
+function toggle_terminal()
+  if vim.fn.win_gotoid(term_win_id) == 1 then
+    -- close
+    cmd("hide")
+  else
+    -- open
+    if vim.fn.bufexists(term_buffer) ~= 1 then
+      -- create window
+      cmd("au TermOpen * setlocal nonumber norelativenumber signcolumn=no")
+      cmd("sp | winc J | res 10 | te")
+      term_win_id = vim.fn.win_getid()
+      term_buffer = vim.fn.bufnr('%')
+    elseif vim.fn.win_gotoid(term_win_id) ~= 1 then
+      -- re-attach window
+      cmd("sb " .. term_buffer .. "| winc J | res 10")
+      term_win_id = vim.fn.win_getid()
+    end
+    cmd("startinsert")
+  end
+end
+vim.keymap.set({"n", "t"}, "<C-a>", toggle_terminal, o)
+

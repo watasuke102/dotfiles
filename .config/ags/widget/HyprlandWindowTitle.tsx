6d18f3dabf34ddcdd80260b5cfc754a5ea0e0daa
watasuke102 <watasuke102@gmail.com>
2025-12-12 10:23:49 +0900

    update: ags>fix error and enable ellipsize for window title

--- .config/ags/widget/HyprlandWindowTitle.tsx
+++ .config/ags/widget/HyprlandWindowTitle.tsx
@@ -1,5 +1,7 @@
 import { bind } from "astal";
 import Hyprland from "gi://AstalHyprland";
+import { Gtk } from "astal/gtk4";
+import Pango from 'gi://Pango?version=1.0';
 
 export function HyprlandWindowTitle() {
   const hyprland = Hyprland.get_default();
@@ -9,11 +11,15 @@ export function HyprlandWindowTitle() {
       <image iconName="window-symbolic" />
       <box hexpand>
         {bind(hyprland, "focused_client").as((client) =>
-          client ? (
-            <label label={bind(client, "title").as(String)} />
+          [client ? (
+            <label
+              label={bind(client, "title").as(e => String(e).replaceAll(/(\n|\u000d)/g, ''))}
+              wrap={false}
+              ellipsize={Pango.EllipsizeMode.END}
+            />
           ) : (
             <label label="<null>" />
-          )
+          )]
         )}
       </box>
     </box>
